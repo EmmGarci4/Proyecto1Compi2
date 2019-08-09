78be0e72f2f122071d15fedc4de5571e60db85cf
EmelyGarcia <garciam.emm@gmail.com>
2019-08-09 13:58:15 -0600

    Gramática chison completa Pendiente la ubicación exacta de las sentencias de importar

--- Proyecto1Compi2/com.Analisis/GramaticaChison.cs
+++ Proyecto1Compi2/com.Analisis/GramaticaChison.cs
@@ -18,6 +18,7 @@ namespace Proyecto1Compi2.com.Analisis
 			RegexBasedTerminal date = new RegexBasedTerminal("date", "'[0-9]{4}-[0-9]{2}-[0-9]{2}'");
 			RegexBasedTerminal time = new RegexBasedTerminal("time", "'[0-9]{2}:[0-9]{2}:[0-9]{2}'");
 			RegexBasedTerminal nombre = new RegexBasedTerminal("nombre", "[a-zA-ZñÑ]+([a-zA-ZñÑ]|_|[0-9])*");
+			RegexBasedTerminal archivo = new RegexBasedTerminal("archivo", "{ (.*)}");
 			CommentTerminal comentario_linea = new CommentTerminal("comentario_linea", "//", "\n", "\r\n");
 			CommentTerminal comentario_bloque = new CommentTerminal("comentario_bloque", "/*", "*/");
 			#endregion
@@ -34,41 +35,145 @@ namespace Proyecto1Compi2.com.Analisis
 			var pr_null = ToTerm("null");
 			var pr_true = ToTerm("true");
 			var pr_false = ToTerm("false");
+			var pr_databases = ToTerm("\"databases\"");
+			var pr_users = ToTerm("\"users\"");
+			var pr_nombre = ToTerm("\"name\"");
+			var pr_passwd = ToTerm("\"password\"");
+			var pr_permisos = ToTerm("\"permissions\"");
+			var pr_data = ToTerm("\"data\"");
+			var pr_cqlType = ToTerm("\"cql-type\"");
+			var pr_columns = ToTerm("\"columns\"");
+			var pr_table = ToTerm("\"table\"");
+			var pr_type = ToTerm("\"type\"");
+			var pr_pk = ToTerm("\"PK\"");
+			var pr_obj = ToTerm("\"OBJECT\"");
+			var pr_attribs = ToTerm("\"ATTRS\"");
+			var pr_proc = ToTerm("\"PROCEDURE\"");
+			var pr_params = ToTerm("\"PARAMETERS\"");
+			var pr_inst = ToTerm("\"INSTR\"");
+			var pr_as = ToTerm("\"AS\"");
+			var pr_in = ToTerm("in");
+			var pr_out = ToTerm("out");
 			#endregion
 
 			#region NoTerm
 			NonTerminal INICIO = new NonTerminal("INICIO"),
+				DATABASE = new NonTerminal("DATABASE"),
+				USER = new NonTerminal("USER"),
+				PASSWD = new NonTerminal("PASSWD"),
+				PERMISOS = new NonTerminal("PERMISOS"),
+				OTRONOMBRE = new NonTerminal("OTRONOMBRE"),
+				LISTA_USERS = new NonTerminal("LISTA_USERS"),
+				LISTA_DATABASES = new NonTerminal("LISTA_DATABASES"),
+				LISTA_OTRONOMBRES = new NonTerminal("LISTA_OTRONOMBRES"),
+				LISTA_OBJDB = new NonTerminal("LISTA_OBJDB"),
+				OBJDB = new NonTerminal("OBJDB"),
+				TABLA = new NonTerminal("TABLA"),
+				COLUMNA = new NonTerminal("COLUMNA"),
+				LISTA_COLUMNAS = new NonTerminal("LISTA_COLUMNAS"),
+				TIPO = new NonTerminal("TIPO"),
+				ISPRIMARY = new NonTerminal("ISPRIMARY"),
 				OBJETO = new NonTerminal("OBJETO"),
-				LISTA_ATRIBUTOS=new NonTerminal("LISTA_ATRIBUTOS"),
-				ATRIBUTO=new NonTerminal("ATRIBUTO"),
-				VALOR=new NonTerminal("VALOR"),
-				LISTA=new NonTerminal("LISTA")
+				LISTA_ATRIBUTOS = new NonTerminal("LISTA_ATRIBUTOS"),
+				ATRIBUTO = new NonTerminal("ATRIBUTO"),
+				DATATABLE = new NonTerminal("DATATABLE"),
+				LISTA_DATATABLE = new NonTerminal("LISTA_DATATABLE"),
+				LISTA_FILA = new NonTerminal("LISTA_FILA"),
+				FILA = new NonTerminal("FILA"),
+				PROCEDIMIENTO = new NonTerminal("PROCEDIMIENTO"),
+				LISTA_PARAMETROS = new NonTerminal("LISTA_PARAMETROS"),
+				PARAMETRO = new NonTerminal("PARAMETRO"),
+				INOUT = new NonTerminal("INOUT"),
+				IMPORTAR = new NonTerminal("IMPORTAR")
 				;
 			#endregion
 
 			#region Gramatcia
 
-			INICIO.Rule = dolar+ OBJETO+ dolar;
+			INICIO.Rule = dolar + menor + pr_databases + igual + cor1 + LISTA_DATABASES + cor2 + coma + pr_users + igual + cor1 + LISTA_USERS + cor2 + mayor + dolar;
 
-			OBJETO.Rule =  menor + LISTA_ATRIBUTOS + mayor ;
+			#region baseDeDatos
 
-			LISTA_ATRIBUTOS.Rule =MakePlusRule(LISTA_ATRIBUTOS,coma,ATRIBUTO);
+			LISTA_DATABASES.Rule = MakeStarRule(LISTA_DATABASES, coma, DATABASE)
+				|IMPORTAR;
 
-			ATRIBUTO.Rule =cadena+igual+VALOR;
+			LISTA_USERS.Rule = MakeStarRule(LISTA_USERS, coma, USER)
+				|IMPORTAR;
 
-			VALOR.Rule = cadena
-				| numero
-				| date
-				| time
-				| pr_null
-				| id
-				|pr_true
-				|pr_false
-				|cor1+LISTA+cor2
+			
+			DATABASE.Rule =menor+ pr_nombre + igual + cadena + coma+ pr_data + igual + cor1 + LISTA_OBJDB + cor2 + mayor;
+
+			LISTA_OBJDB.Rule =MakeStarRule(LISTA_OBJDB,coma,OBJDB)
+				|IMPORTAR;
+
+			OBJDB.Rule =TABLA
 				|OBJETO
-				;
+				|PROCEDIMIENTO;
 
-			LISTA.Rule = MakeStarRule(LISTA,coma,VALOR);
+			IMPORTAR.Rule =dolar+archivo+dolar;
+			#endregion
+
+			#region Tabla
+
+			TABLA.Rule =menor+ pr_cqlType + igual + pr_table + coma+ pr_nombre + igual + cadena + coma + pr_columns + igual + cor1 + LISTA_COLUMNAS + cor2 + coma+pr_data+igual+cor1+LISTA_FILA+cor2+mayor;
+
+			LISTA_FILA.Rule =MakeStarRule(LISTA_FILA,coma,FILA)
+				|IMPORTAR;
+
+			FILA.Rule =menor+LISTA_DATATABLE+mayor;
+
+			LISTA_DATATABLE.Rule = MakePlusRule(LISTA_DATATABLE,coma,DATATABLE);
+
+			DATATABLE.Rule = cadena + igual + cadena
+				|cadena+igual+numero
+				|cadena+igual+date
+				|cadena+igual+time
+				|cadena+igual+pr_true
+				|cadena+igual+pr_false;
+
+			LISTA_COLUMNAS.Rule = MakeStarRule(LISTA_COLUMNAS,coma,COLUMNA)
+				|IMPORTAR;
+
+			COLUMNA.Rule =menor+ pr_nombre + igual + cadena + coma+TIPO+coma+ISPRIMARY+mayor;
+
+			TIPO.Rule =pr_type+igual+cadena;
+
+			ISPRIMARY.Rule = pr_pk + igual + pr_true
+				| pr_pk + igual + pr_false;
+			#endregion
+
+			#region Objetos
+			OBJETO.Rule =menor+ pr_cqlType + igual + pr_obj+coma + pr_nombre + igual + cadena + coma+pr_attribs+igual+cor1+LISTA_ATRIBUTOS+cor2+mayor;
+
+			LISTA_ATRIBUTOS.Rule =MakeStarRule(LISTA_ATRIBUTOS,coma,ATRIBUTO);
+
+			ATRIBUTO.Rule =menor+ pr_nombre + igual + cadena + coma+TIPO+mayor;
+
+			#endregion
+
+			#region Usuarios
+			
+			USER.Rule = menor+ pr_nombre + igual + cadena + coma+PASSWD+coma+PERMISOS+mayor;
+
+			PASSWD.Rule =pr_passwd+igual+cadena;
+
+			PERMISOS.Rule =pr_permisos+igual+cor1+LISTA_OTRONOMBRES+cor2;
+
+			LISTA_OTRONOMBRES.Rule = MakeStarRule(LISTA_OTRONOMBRES,coma,OTRONOMBRE);
+
+			OTRONOMBRE.Rule =menor+ pr_nombre + igual + cadena+mayor;
+			#endregion
+
+			#region procedimiento
+			PROCEDIMIENTO.Rule = menor + pr_cqlType + igual + pr_proc + coma + pr_nombre + igual + cadena + coma + pr_params+igual+cor1+LISTA_PARAMETROS+cor2 + coma + pr_inst + igual + cadena + mayor;
+
+			LISTA_PARAMETROS.Rule =MakeStarRule(LISTA_PARAMETROS,coma,PARAMETRO);
+
+			PARAMETRO.Rule = menor+ pr_nombre + igual + cadena + coma + TIPO + coma + pr_as + igual+INOUT+mayor;
+
+			INOUT.Rule = pr_in | pr_out;
+
+			#endregion
 
 			#endregion
 
@@ -76,7 +181,9 @@ namespace Proyecto1Compi2.com.Analisis
 			//NO TERMINAL DE INICIO
 			this.Root = INICIO;
 			//PALABRAS RESERVADAS
-			MarkReservedWords(pr_null.ToString(),pr_true.ToString(),pr_false.ToString());
+			MarkReservedWords(pr_nombre.ToString(),pr_null.ToString(),pr_true.ToString(),pr_false.ToString(),pr_passwd.ToString(),pr_permisos.ToString(),pr_data.ToString(),
+				pr_cqlType.ToString(),pr_columns.ToString(),pr_table.ToString(),pr_type.ToString(),pr_pk.ToString(),pr_obj.ToString(),pr_attribs.ToString(),pr_proc.ToString(),
+				pr_params.ToString(),pr_inst.ToString(),pr_as.ToString(),pr_in.ToString(),pr_out.ToString(),pr_databases.ToString(),pr_users.ToString());
 			//NODOS A OMITIR
 			MarkTransient();
 			//TERMINALES IGNORADO
