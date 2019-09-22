00803d37c9f6440bfa3278b6058dc0d07589cc2d
EmelyGarcia <garciam.emm@gmail.com>
2019-09-22 16:08:35 -0600

    validando objetos en chison

--- Proyecto1Compi2/com.Analisis/GeneradorDB.cs
+++ Proyecto1Compi2/com.Analisis/GeneradorDB.cs
@@ -14,7 +14,7 @@ namespace Proyecto1Compi2.com.Analisis
 	class GeneradorDB
 	{
 		static List<Error> erroresChison = new List<Error>();
-		static Dictionary<int,int> intervalos = new Dictionary<int, int>();
+		static Dictionary<int, int> intervalos = new Dictionary<int, int>();
 
 		public static List<Error> ErroresChison { get => erroresChison; set => erroresChison = value; }
 
@@ -67,12 +67,12 @@ namespace Proyecto1Compi2.com.Analisis
 					if (t1 != null)
 					{
 						texto = texto.Replace(match2.Value, t1);
-						intervalos.Add(contadorLineas, t1.Split('\n').Length+contadorLineas);
+						intervalos.Add(contadorLineas, t1.Split('\n').Length + contadorLineas);
 					}
 					else
 					{
 						texto = texto.Replace(linea, String.Empty);
-						erroresChison.Add(new Error(TipoError.Advertencia, "El archivo '"+match2.Value+"' no existe en la carpeta data",
+						erroresChison.Add(new Error(TipoError.Advertencia, "El archivo '" + match2.Value + "' no existe en la carpeta data",
 							contadorLineas, 1,
 							Datos.GetDate(), Datos.GetTime()));
 					}
@@ -100,14 +100,15 @@ namespace Proyecto1Compi2.com.Analisis
 			{
 				int linea = GetLineaError(error.Linea);
 				String mensajeExtra = "";
-				if (linea!=error.Linea) {
-					mensajeExtra = "(línea:"+GetLineaRealError(error.Linea) +" en archivo)";
+				if (linea != error.Linea)
+				{
+					mensajeExtra = "(línea:" + GetLineaRealError(error.Linea) + " en archivo)";
 				}
 				valores.Clear();
 				int contador = Analizador.Errors.BuscarColumna("numero").GetUltimoValorCounter();
 				valores.Enqueue(contador + 1);
 				valores.Enqueue(error.Tipo.ToString());
-				valores.Enqueue(error.Mensaje+mensajeExtra);
+				valores.Enqueue(error.Mensaje + mensajeExtra);
 				valores.Enqueue(linea);
 				valores.Enqueue(error.Columna);
 				valores.Enqueue(new MyDateTime(TipoDatoDB.DATE, DateTime.Parse(error.Fecha)));
@@ -126,7 +127,7 @@ namespace Proyecto1Compi2.com.Analisis
 				{
 					if (line >= intervalo.Key && line <= intervalo.Value)
 					{
-						return line-intervalo.Key;
+						return line - intervalo.Key;
 					}
 				}
 			}
@@ -135,11 +136,13 @@ namespace Proyecto1Compi2.com.Analisis
 
 		private static int GetLineaError(int line)
 		{
-			if (intervalos.Count>0) {
+			if (intervalos.Count > 0)
+			{
 
 				foreach (KeyValuePair<int, int> intervalo in intervalos)
-				{			
-					if (line>intervalo.Key && line<intervalo.Value) {
+				{
+					if (line > intervalo.Key && line < intervalo.Value)
+					{
 						return intervalo.Key;
 					}
 				}
@@ -419,11 +422,11 @@ namespace Proyecto1Compi2.com.Analisis
 					switch (t)
 					{
 						case TipoObjeto.Objeto:
-							UserType ustype = GetUserType(nodo,lista);
+							UserType ustype = GetUserType(nodo, lista);
 							if (ustype != null) lista.Add(ustype);
 							break;
 						case TipoObjeto.Procedimiento:
-							Procedimiento proc = GetProcedimiento(nodo,lista);
+							Procedimiento proc = GetProcedimiento(nodo, lista);
 							if (proc != null) lista.Add(proc);
 							break;
 						case TipoObjeto.Tabla:
@@ -526,17 +529,19 @@ namespace Proyecto1Compi2.com.Analisis
 											}
 											else
 											{
-											erroresChison.Add(new Error(TipoError.Semantico,
-											"El objeto '" + col.Tipo.ToString() + "' no existe",
-											raiz.Span.Location.Line, raiz.Span.Location.Column,
-											Datos.GetDate(),
-											Datos.GetTime()));
+												erroresChison.Add(new Error(TipoError.Semantico,
+												"El objeto '" + col.Tipo.ToString() + "' no existe",
+												raiz.Span.Location.Line, raiz.Span.Location.Column,
+												Datos.GetDate(),
+												Datos.GetTime()));
 											}
 										}
 										else
 										{
-											if (Datos.IsLista(col.Tipo.Tipo)) {
-												if (!ValidarInstanciaLista(col.Tipo, db, raiz.Span.Location.Line, raiz.Span.Location.Column)) {
+											if (Datos.IsLista(col.Tipo.Tipo))
+											{
+												if (!ValidarInstanciaLista(col.Tipo, db, raiz.Span.Location.Line, raiz.Span.Location.Column))
+												{
 													continue;
 												}
 											}
@@ -583,7 +588,7 @@ namespace Proyecto1Compi2.com.Analisis
 								List<FilaDatos> datos = GetDatosTabla(nodo.ChildNodes.ElementAt(1), db);
 								foreach (FilaDatos fila in datos)
 								{
-									InsertarEnTabla(tabla, fila.Datos,nodo.Span.Location.Line,nodo.Span.Location.Column);
+									InsertarEnTabla(tabla, fila.Datos, nodo.Span.Location.Line, nodo.Span.Location.Column);
 								}
 							}
 							else
@@ -626,7 +631,7 @@ namespace Proyecto1Compi2.com.Analisis
 			return null;
 		}
 
-		private static bool ValidarInstanciaLista(TipoObjetoDB tipoInstancia, List<object> objetos,int linea,int columna)
+		private static bool ValidarInstanciaLista(TipoObjetoDB tipoInstancia, List<object> objetos, int linea, int columna)
 		{
 			switch (tipoInstancia.Tipo)
 			{
@@ -635,9 +640,9 @@ namespace Proyecto1Compi2.com.Analisis
 					TipoObjetoDB tipoAdentro = Datos.GetTipoObjetoDBPorCadena(tipoInstancia.Nombre);
 					if (Datos.IsLista(tipoAdentro.Tipo))
 					{
-						if (!ValidarInstanciaLista(tipoAdentro, objetos,linea,columna))
+						if (!ValidarInstanciaLista(tipoAdentro, objetos, linea, columna))
 						{
-								return false;
+							return false;
 						}
 						return true;
 					}
@@ -648,10 +653,10 @@ namespace Proyecto1Compi2.com.Analisis
 					else
 					{
 						//comprobar que exista el objeto
-						if (!ExisteUt(tipoAdentro.Nombre,objetos))
+						if (!ExisteUt(tipoAdentro.Nombre, objetos))
 						{
 							erroresChison.Add(new Error(TipoError.Semantico,
-								"El objeto '"+tipoAdentro.Nombre+"' no existe",
+								"El objeto '" + tipoAdentro.Nombre + "' no existe",
 								  linea, columna,
 								  Datos.GetDate(), Datos.GetTime()));
 							return false;
@@ -667,7 +672,7 @@ namespace Proyecto1Compi2.com.Analisis
 					tipoAdentro = Datos.GetTipoObjetoDBPorCadena(tipos[1]);
 					if (Datos.IsLista(tipoAdentro.Tipo))
 					{
-						if (!ValidarInstanciaLista(tipoAdentro, objetos,linea,columna))
+						if (!ValidarInstanciaLista(tipoAdentro, objetos, linea, columna))
 						{
 							return false;
 						}
@@ -694,7 +699,7 @@ namespace Proyecto1Compi2.com.Analisis
 			return false;
 		}
 
-		private static void InsertarEnTabla(Tabla tab, List<ParDatos> valores,int linea,int columna)
+		private static void InsertarEnTabla(Tabla tab, List<ParDatos> valores, int linea, int columna)
 		{
 			if (valores.Count == tab.Columnas.Count)
 			{
@@ -706,16 +711,17 @@ namespace Proyecto1Compi2.com.Analisis
 				{
 					ParDatos respuesta = valores.ElementAt(indiceDatos);
 
-						if (Datos.IsTipoCompatibleParaAsignar(cl.Tipo, respuesta.Valor))
-						{
-							valoresAInsertar.Enqueue(respuesta.Valor);
-						}
-						else
-						{
+					if (Datos.IsTipoCompatibleParaAsignar(cl.Tipo, respuesta.Valor))
+					{
+						valoresAInsertar.Enqueue(respuesta.Valor);
+					}
+					else
+					{
 						TipoObjetoDB tipoRes = Datos.GetTipoObjetoDB(respuesta.Valor);
 						if (Datos.IsLista(tipoRes.Tipo))
 						{
-							if (tipoRes.Tipo!=TipoDatoDB.MAP_OBJETO && tipoRes.Tipo!=TipoDatoDB.MAP_PRIMITIVO) {
+							if (tipoRes.Tipo != TipoDatoDB.MAP_OBJETO && tipoRes.Tipo != TipoDatoDB.MAP_PRIMITIVO)
+							{
 								CollectionListCql colection = (CollectionListCql)respuesta.Valor;
 								if (tipoRes.Nombre == "null")
 								{
@@ -726,10 +732,12 @@ namespace Proyecto1Compi2.com.Analisis
 								{
 									if (cl.Tipo.Nombre.Equals(colection.TipoDato.Nombre))
 									{
-										if (cl.Tipo.Tipo == TipoDatoDB.LISTA_OBJETO || cl.Tipo.Tipo == TipoDatoDB.LISTA_PRIMITIVO) {
+										if (cl.Tipo.Tipo == TipoDatoDB.LISTA_OBJETO || cl.Tipo.Tipo == TipoDatoDB.LISTA_PRIMITIVO)
+										{
 											colection.IsLista = true;
 											valoresAInsertar.Enqueue(colection);
-										} else if (cl.Tipo.Tipo == TipoDatoDB.SET_OBJETO || cl.Tipo.Tipo == TipoDatoDB.SET_PRIMITIVO)
+										}
+										else if (cl.Tipo.Tipo == TipoDatoDB.SET_OBJETO || cl.Tipo.Tipo == TipoDatoDB.SET_PRIMITIVO)
 										{
 											colection.IsLista = false;
 											valoresAInsertar.Enqueue(colection);
@@ -745,7 +753,7 @@ namespace Proyecto1Compi2.com.Analisis
 								  linea, columna,
 								  Datos.GetDate(), Datos.GetTime()));
 						}
-						}
+					}
 					indiceColumnas++;
 					indiceDatos++;
 				}
@@ -756,13 +764,14 @@ namespace Proyecto1Compi2.com.Analisis
 					object correcto = tab.ValidarPk(valoresAInsertar, linea, columna);
 					if (correcto.GetType() == typeof(ThrowError))
 					{
-						erroresChison.Add(new Error((ThrowError)correcto,true));
+						erroresChison.Add(new Error((ThrowError)correcto, true));
 					}
-					else {
+					else
+					{
 						//LLENANDO TABLA
 						tab.AgregarValores(valoresAInsertar);
 					}
-					
+
 				}
 			}
 			else
@@ -795,7 +804,8 @@ namespace Proyecto1Compi2.com.Analisis
 				if (nodo.Term.Name == "OBJETO")
 				{
 					FilaDatos fil = GetFilaDatos(nodo, db);
-					if (fil!=null){
+					if (fil != null)
+					{
 						datos.Add(fil);
 					}
 				}
@@ -821,7 +831,8 @@ namespace Proyecto1Compi2.com.Analisis
 					fila.Datos.Add(new ParDatos(nodis.ChildNodes.ElementAt(0).Token.ValueString.ToLower(),
 					val));
 				}
-				else {
+				else
+				{
 					return null;
 				}
 			}
@@ -906,7 +917,7 @@ namespace Proyecto1Compi2.com.Analisis
 						{
 							if (Datos.IsTipoCompatibleParaAsignar(tipo, exp))
 							{
-								object nuevoVal = CasteoImplicito(tipo, exp,linea, column);
+								object nuevoVal = CasteoImplicito(tipo, exp, linea, column);
 								object posibleError = collection.AddItem(nuevoVal, linea, column);
 								if (posibleError != null)
 								{
@@ -928,13 +939,14 @@ namespace Proyecto1Compi2.com.Analisis
 					}
 					else
 					{
-						return new CollectionListCql(new TipoObjetoDB(TipoDatoDB.LISTA_OBJETO,"null"),true);
+						return new CollectionListCql(new TipoObjetoDB(TipoDatoDB.LISTA_OBJETO, "null"), true);
 					}
 				case "OBJETO":
 					FilaDatos atributos = GetFilaDatos(parseTreeNode, db);
 					List<object> expresiones = new List<object>();
 					List<String> attrs = new List<string>();
-					if (atributos!=null) {
+					if (atributos != null)
+					{
 						foreach (ParDatos par in atributos.Datos)
 						{
 							attrs.Add(par.Nombre);
@@ -1028,7 +1040,9 @@ namespace Proyecto1Compi2.com.Analisis
 													}
 												}
 											}
-										} else if (atributo.Value.Tipo==TipoDatoDB.OBJETO && tipoRes.Tipo==TipoDatoDB.NULO) {
+										}
+										else if (atributo.Value.Tipo == TipoDatoDB.OBJETO && tipoRes.Tipo == TipoDatoDB.NULO)
+										{
 											Objeto obj = new Objeto(usert, true);
 											nuevaInstancia.Atributos.Add(atributo.Key, obj);
 										}
@@ -1070,7 +1084,8 @@ namespace Proyecto1Compi2.com.Analisis
 		private static object GetMap(ParseTreeNode parseTreeNode)
 		{
 			CollectionMapCql map = new CollectionMapCql(null, null);
-			if (parseTreeNode.ChildNodes.Count>0) {
+			if (parseTreeNode.ChildNodes.Count > 0)
+			{
 				object llave = GetDatoP(parseTreeNode.ChildNodes.ElementAt(0).ChildNodes.ElementAt(0).ChildNodes.ElementAt(0));
 				object valor = Datos.GetValor(parseTreeNode.ChildNodes.ElementAt(0).ChildNodes.ElementAt(1).Token.ValueString);
 				map.TipoLlave = Datos.GetTipoObjetoDB(llave);
@@ -1080,8 +1095,10 @@ namespace Proyecto1Compi2.com.Analisis
 					llave = GetDatoP(nodo.ChildNodes.ElementAt(0).ChildNodes.ElementAt(0));
 					valor = Datos.GetValor(nodo.ChildNodes.ElementAt(1).Token.ValueString);
 					object posibleError = map.AddItem(llave, valor, nodo.Span.Location.Line, nodo.Span.Location.Column);
-					if (posibleError!=null) {
-						if (posibleError.GetType()==typeof(ThrowError)) {
+					if (posibleError != null)
+					{
+						if (posibleError.GetType() == typeof(ThrowError))
+						{
 							erroresChison.Add(new Error((ThrowError)posibleError, true));
 						}
 					}
@@ -1138,14 +1155,15 @@ namespace Proyecto1Compi2.com.Analisis
 					return di;
 			}
 			return null;
-			}
+		}
 
 		private static UserType GetUt(List<string> attrs, List<object> tab)
 		{
 			List<UserType> usertpypes = GetUserTypesLista(tab);
 			foreach (UserType ut in usertpypes)
 			{
-				if (ut.Atributos.Count==attrs.Count) {
+				if (ut.Atributos.Count == attrs.Count)
+				{
 					bool contienetodo = true;
 					int contadorAt = 0;
 					foreach (KeyValuePair<string, TipoObjetoDB> atributo in ut.Atributos)
@@ -1299,7 +1317,7 @@ namespace Proyecto1Compi2.com.Analisis
 			return columnas;
 		}
 
-		private static Procedimiento GetProcedimiento(ParseTreeNode raiz,List<object> db)
+		private static Procedimiento GetProcedimiento(ParseTreeNode raiz, List<object> db)
 		{
 			Procedimiento proc = new Procedimiento(raiz.Span.Location.Line, raiz.Span.Location.Column);
 			string t = null;
@@ -1400,16 +1418,18 @@ namespace Proyecto1Compi2.com.Analisis
 															proc.Parametros.Add(par);
 														}
 													}
-													else {
+													else
+													{
 														proc.Parametros.Add(par);
 													}
 												}
-												else {
+												else
+												{
 													proc.Parametros.Add(par);
 												}
-												
+
 											}
-											
+
 										}
 										else
 										{
@@ -1449,7 +1469,7 @@ namespace Proyecto1Compi2.com.Analisis
 											{
 												if (Datos.IsLista(par.Tipo.Tipo))
 												{
-													if (ValidarInstanciaLista(par.Tipo, db,raiz.Span.Location.Line, raiz.Span.Location.Column))
+													if (ValidarInstanciaLista(par.Tipo, db, raiz.Span.Location.Line, raiz.Span.Location.Column))
 													{
 														proc.Retornos.Add(par);
 													}
@@ -1817,7 +1837,7 @@ namespace Proyecto1Compi2.com.Analisis
 			return false;
 		}
 
-		private static UserType GetUserType(ParseTreeNode raiz,List<object> db)
+		private static UserType GetUserType(ParseTreeNode raiz, List<object> db)
 		{
 			UserType user = new UserType();
 			string t = null;
@@ -1882,7 +1902,7 @@ namespace Proyecto1Compi2.com.Analisis
 						{
 							if (user.Atributos == null)
 							{
-								Dictionary<string, TipoObjetoDB> resultado = GetListaAtributos(nodo.ChildNodes.ElementAt(1),db);
+								Dictionary<string, TipoObjetoDB> resultado = GetListaAtributos(nodo.ChildNodes.ElementAt(1), db,user.Nombre);
 								if (resultado != null)
 									user.Atributos = resultado;
 							}
@@ -1929,7 +1949,7 @@ namespace Proyecto1Compi2.com.Analisis
 			return null;
 		}
 
-		private static Dictionary<string, TipoObjetoDB> GetListaAtributos(ParseTreeNode parseTreeNode,List<object> db)
+		private static Dictionary<string, TipoObjetoDB> GetListaAtributos(ParseTreeNode parseTreeNode, List<object> db, string nombreUt)
 		{
 			Dictionary<string, TipoObjetoDB> dic = new Dictionary<string, TipoObjetoDB>();
 
@@ -1978,19 +1998,37 @@ namespace Proyecto1Compi2.com.Analisis
 										TipoObjetoDB tipodato = Datos.GetTipoObjetoDBPorCadena(raiz.ChildNodes.ElementAt(1).Token.ValueString.ToLower());
 										if (tipodato.Tipo == TipoDatoDB.OBJETO)
 										{
-										//	if (ExisteUt(tipodato.Nombre, db))
-										//	{
-												tipo = tipodato.ToString() ;
-										//	}
-										/*	else
+											if (ExisteUt(tipodato.Nombre, db))
 											{
-												
-												erroresChison.Add(new Error(TipoError.Semantico,
-												"El objeto '" + tipodato.ToString() + "' no existe",
-												raiz.Span.Location.Line, raiz.Span.Location.Column,
-												Datos.GetDate(),
-												Datos.GetTime()));
-											}*/
+												tipo = tipodato.ToString();
+											}
+											else
+											{
+												if (nombreUt != null)
+												{
+													if (nombreUt == tipodato.Nombre)
+													{
+														tipo = tipodato.ToString();
+													}
+													else
+													{
+														erroresChison.Add(new Error(TipoError.Semantico,
+															"El objeto '" + tipodato.ToString() + "' no existe",
+															raiz.Span.Location.Line, raiz.Span.Location.Column,
+															Datos.GetDate(),
+															Datos.GetTime()));
+													}
+												}
+												else
+												{
+													erroresChison.Add(new Error(TipoError.Semantico,
+													"El objeto '" + tipodato.ToString() + "' no existe",
+													raiz.Span.Location.Line, raiz.Span.Location.Column,
+													Datos.GetDate(),
+													Datos.GetTime()));
+												}
+
+											}
 										}
 										else
 										{
@@ -2001,10 +2039,11 @@ namespace Proyecto1Compi2.com.Analisis
 													tipo = tipodato.ToString();
 												}
 											}
-											else {
+											else
+											{
 												tipo = tipodato.ToString();
 											}
-											
+
 										}
 									}
 									else
@@ -2215,22 +2254,25 @@ namespace Proyecto1Compi2.com.Analisis
 							if (usuario.Permisos == null)
 							{
 								List<string> resultado = GetListaPermisosUsuario(nodo.ChildNodes.ElementAt(1));
-								if (resultado != null) {
+								if (resultado != null)
+								{
 									List<string> permisos = new List<string>();
-									foreach (string permiso in resultado) {
+									foreach (string permiso in resultado)
+									{
 										if (Analizador.ExisteDB(permiso))
 										{
 											permisos.Add(permiso);
 										}
-										else {
+										else
+										{
 											erroresChison.Add(new Error(TipoError.Semantico, "No se puede asignar el permiso sobre la base de datos '" + permiso + "' si no existe",
-														nodo.Span.Location.Line,nodo.Span.Location.Column, 
+														nodo.Span.Location.Line, nodo.Span.Location.Column,
 														Datos.GetDate(), Datos.GetTime()));
 										}
 									}
 									usuario.Permisos = resultado;
 								}
-									
+
 							}
 							else
 							{
